0372174f0829172db144aedc958d9284d2ede588
Akash Singh <akashsunilsingh5555@gmail.com>
2024-07-18 23:17:00 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -17,14 +17,26 @@ if(navigator.geolocation){
     );
 }
 
-const map = L.map("map").setView([0,0], 10);
+const map = L.map("map").setView([0,0], 16);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "OpenStreetMap"
 }).addTo(map);
 
-const marker = {};
+const markers = {};
 
 socket.on("receive-location", (data) => {
     const { id, latitude, longitude } = data;
     map.setView([latitude, longitude], 16);
+    if (markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+
+socket.on("user-disconnected", (id) => {
+    if(markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
 });

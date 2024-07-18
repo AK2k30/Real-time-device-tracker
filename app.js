d8f5c4b18c32d811ce2dc585e5cc940093ccc276
Akash Singh <akashsunilsingh5555@gmail.com>
2024-07-18 23:17:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,10 @@ io.on("connection", function(socket){
     socket.on("send-location", function(data){
         io.emit("receive-location", {id: socket.id, ...data});
     });
-    console.log("connected");
+
+    socket.on("disconnect", function(){
+        io.emit("user-disconnect")
+    })
 });
 
 app.get("/", function (req, res){
